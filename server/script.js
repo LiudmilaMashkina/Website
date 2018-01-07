ecbb6e7af23e4aee29c72935c0c27b285ce4b059
Liudmila Mashkina <liudmilamashkina@gmail.com>
2018-01-06 22:28:28 -0800

    font

--- server/script.js
+++ server/script.js
@@ -4,7 +4,7 @@ function scale_tree()
     $img_natural_w = $img[0].naturalWidth
     $img_natural_h = $img[0].naturalHeight
     
-    $img_h = $('#christmas_tree_anchor').height() * 1.3
+    $img_h = $('#christmas_tree_anchor').height() * 1.7
     $img_w = $img_h * $img_natural_w / $img_natural_h
     $('#christmas_tree').width($img_w).height($img_h)
 };
